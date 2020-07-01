387305934c236cfe232768fd5fa3691eb78a141c
louis-68 <67467839+louis-68@users.noreply.github.com>
2020-07-01 23:08:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var port=process.env.PORT || 8080
 app.use(express.static(__dirname));
 app.get("/",function(req,res){
-    res.render("index");
+    res.render("gerald anna");
 })
 app.listen(port,function(){
     console.log("app running")
